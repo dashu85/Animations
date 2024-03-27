e1775dea857f2b1dd20d4c3de67b0e37f66295ec
Marcus Benoit <benoit.marcus@gmail.com>
2024-03-27 19:01:18 +0100

    added animation and blur effect

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
         .foregroundColor(.white)
         .clipShape(.circle)
         .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount)
     }
 }
 
