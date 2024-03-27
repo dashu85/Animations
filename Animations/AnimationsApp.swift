2f57663fe6778511c7d2651fee47a13a9d613a79
Marcus Benoit <benoit.marcus@gmail.com>
2024-03-27 18:47:37 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Marcus Benoit on 27.03.24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
