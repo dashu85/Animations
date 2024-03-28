998dd3ae0f6ea5792c2f6864af8ceb5a7f5ad945
Marcus Benoit <benoit.marcus@gmail.com>
2024-03-28 13:09:38 +0100

    Implemented Animated Bindings

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount = 1.0
+    @State private var animationAmount2 = 1.0
     
     var body: some View {
+        print(animationAmount2)
+        
+        Spacer()
+        
         Button("Tap me!") {
             animationAmount += 1
         }
@@ -20,7 +25,50 @@ struct ContentView: View {
         .clipShape(.circle)
         .scaleEffect(animationAmount)
         .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        .animation(.easeInOut(duration: 2)
+            .repeatForever(autoreverses: true),
+        value: animationAmount)
+        
+        Spacer()
+        
+        Button("Tap me!") {
+            // animationAmount += 1
+        }
+        .padding(50)
+        .background(.blue)
+        .foregroundColor(.white)
+        .clipShape(.circle)
+        .overlay(
+            Circle()
+                .stroke(.blue)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(.easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                value: animationAmount)
+        )
+        .onAppear {
+            animationAmount = 2
+        }
+        
+        Spacer()
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount2.animation(), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap me!!") {
+                animationAmount2 += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount2)
+            
+            Spacer()
+        }
     }
 }
 
