60212008d21b28eff7e2aef26bf3192c11c0ec81
Marcus Benoit <benoit.marcus@gmail.com>
2024-04-02 12:25:32 +0200

    order of animations matter, how to disable animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     @State private var animationAmount2 = 1.0
+    @State private var animationAmount3 = 0.0
+    
+    @State private var enabled = false
     
     var body: some View {
-        print(animationAmount2)
+        //print(animationAmount2)
         
         Spacer()
         
+        /*
         Button("Tap me!") {
             animationAmount += 1
         }
@@ -52,13 +56,14 @@ struct ContentView: View {
         }
         
         Spacer()
+         */
         
         // Animated Bindings
         /* 
          SwiftUI is examing the state before the binding changes and after the binding has changed and animated what happens between these two changes.
          
          In the following the animation modifier is attached directly onto the value and not the view!
-         */
+         
         
         return VStack {
             Stepper("Scale amount", value: $animationAmount2.animation(
@@ -76,9 +81,56 @@ struct ContentView: View {
             .foregroundColor(.white)
             .clipShape(.circle)
             .scaleEffect(animationAmount2)
-            
-            Spacer()
+         
+         */
+        
+        /*
+        Button("Tap me") {
+//            withAnimation {
+//                animationAmount3 += -360
+//            }
+            withAnimation(.spring(duration: 1, bounce: 0.3)) {
+                animationAmount3 += -360
+            }
         }
+        .padding(40)
+        .background(.black)
+        .foregroundColor(.yellow)
+        .font(.headline.bold())
+        .clipShape(.circle)
+        .rotation3DEffect(
+            .degrees(animationAmount3),
+            axis: (x: 1, y: 0, z: 1)
+        )
+            */
+        
+        Button("Tap me Bitch!") {
+            enabled.toggle()
+        }
+        .background(enabled ? .blue : .black)
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .yellow)
+        .foregroundColor(enabled ? .white : .yellow)
+        .animation(.default, value: enabled)
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+        
+        Spacer()
+        
+        // in order to disable animations you can pass 'nil' into your modifier.
+        
+        Button("Tap me Bitch!") {
+            enabled.toggle()
+        }
+        .background(enabled ? .blue : .black)
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .yellow)
+        .foregroundColor(enabled ? .white : .yellow)
+        .animation(nil, value: enabled)
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+        
+        Spacer()
     }
 }
 
